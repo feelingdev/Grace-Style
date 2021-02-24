8eb215a32f909dae641002a76e4c79851a24e901
feelingdev <49396518+feelingdev@users.noreply.github.com>
2021-02-25 00:49:35 +0800

    Grace-Style Initial Change

---
+++ App/Grace-Style.App/Grace-Style.App/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Grace_Style.App.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
